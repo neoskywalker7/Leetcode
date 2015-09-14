ce702fd515322fe0a4ebe0e153de0cec6f3a1876
ZHEN LONG <zl1399@nyu.edu>
2015-09-14 17:59:44 -0400

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,10 @@
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
