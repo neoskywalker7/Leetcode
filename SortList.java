e712fdf450bda8924cdf724b9e1da31a08b87542
ZHEN LONG <zl1399@nyu.edu>
2015-09-16 22:39:14 -0400

    Create SortList.java

---
+++ SortList.java
@@ -0,0 +1,44 @@
+ private ListNode findMiddle(ListNode head) { //这个用快慢指针找中点的方法非常巧妙
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }    
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (head1 != null && head2 != null) {//这个的思想就是左右接比较大小，改变tail来把所有数连起来
+            if (head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = findMiddle(head);
+
+        ListNode right = sortList(mid.next);
+        mid.next = null;//这部操作很关键    我要好好想一想  可能是让它只有一个元素把
+        ListNode left = sortList(head);
+
+        return merge(left, right);
+    }
